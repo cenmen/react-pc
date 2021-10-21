b7f5dbc3b46aa266d506ce54b7f31d8ce18b048d
liangtiecheng <>
2021-10-21 10:57:08 +0800

    使用xlsx充当数据库

--- node/index.js
+++ node/index.js
@@ -3,30 +3,35 @@ const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const fs = require('fs')
-const path = require('path')
+const xlsx = require('node-xlsx')
 const app = express()
 app.use(cors({ credentials: true, origin: true }))
 app.use(bodyParser.json())
 app.use(cookieParser())
 
-const ACCOUNT = [
-  {
-    username: '123456',
-    password: '123456',
-    role: 'normal'
-  },
-  {
-    username: 'admin',
-    password: 'admin',
-    role: 'admin'
-  }
-]
+const getTableData = (tableName) => {
+  const { data } = xlsx.parse(`${__dirname}/data/${tableName}.xlsx`)[0]
+  const title = data.shift()
+  /* 格式化数组对象形式 */
+  return data.map((row) => {
+    const item = {}
+    row.forEach((val, index) => {
+      item[title[index]] = val
+    })
+    return item
+  })
+}
 
-const JSON_PATH = path.join(__dirname, './data/dataSource.json')
+const setTableData = (tableName, data = []) => {
+  /* 将数组对象格式回目标数组格式 */
+  const title = Object.keys(data[0])
+  const list = data.map((item) => Object.values(item))
+  const buffer = xlsx.build([{ name: tableName, data: [title, ...list] }])
+  fs.writeFileSync(`${__dirname}/data/${tableName}.xlsx`, buffer)
+}
 
 app.all('/*', function (req, res, next) {
   const { url, cookies } = req
-  console.log('==> all', url, cookies)
   if (url.includes('login') || cookies.role) {
     next()
   } else {
@@ -37,9 +42,9 @@ app.all('/*', function (req, res, next) {
 app.post('/login', function (req, res) {
   // console.log('==> req', req.cookies, req.signedCookies, req.query, req.params, req.body)
   const { username, password } = req.body
-  const { role } = ACCOUNT.find((val) => val.username === username && val.password === password)
-  // console.log('==> role', role)
-  res.cookie('role', role, { maxAge: 30*60*1000, httpOnly: true, path: '/' })
+  const data = getTableData('user')
+  const { role } = data.find((val) => val.username === username && val.password === password)
+  res.cookie('role', role, { maxAge: 30 * 60 * 1000, httpOnly: true, path: '/' })
   res.send({ role })
 })
 
@@ -49,54 +54,46 @@ app.get('/role', function (req, res) {
 })
 
 app.get('/list', function (req, res) {
-  let raw = fs.readFileSync(JSON_PATH)
-  let list = JSON.parse(raw)
-  res.status(200).send(list)
+  const data = getTableData('phone')
+  res.status(200).send(data)
 })
 
 app.get('/detail', function (req, res) {
-  const {id} = req.query
-  let raw = fs.readFileSync(JSON_PATH)
-  let list = JSON.parse(raw)
-  const item = list.find(item => item.id === id)
+  const { id } = req.query
+  const data = getTableData('phone')
+  const item = data.find((item) => item.id === id)
   res.status(200).send(item)
 })
 
 app.post('/add', function (req, res) {
   const { id, name, price, brand } = req.body
-  let raw = fs.readFileSync(JSON_PATH)
-  let list = JSON.parse(raw)
-  if (list.some(item => item.id === id)) {
-    return res.status(500).send({message: '已存在相同id的数据'})
+  const data = getTableData('phone')
+  if (data.some((item) => item.id === id)) {
+    return res.status(500).send({ message: '已存在相同id的数据' })
   }
-  list.push({ id, name, price, brand })
-  let data = JSON.stringify(list)
-  fs.writeFileSync(JSON_PATH, data)
-  res.status(200).send({message: '添加成功'})
+  data.push({ id, name, price, brand })
+  setTableData('phone', data)
+  res.status(200).send({ message: '添加成功' })
 })
 
 app.post('/update', function (req, res) {
   const { id, name, price, brand } = req.body
-  let raw = fs.readFileSync(JSON_PATH)
-  let list = JSON.parse(raw)
-  const item = list.find(item => item.id === id)
+  const data = getTableData('phone')
+  const item = data.find((item) => item.id === id)
   item.name = name
   item.price = price
   item.brand = brand
-  let data = JSON.stringify(list)
-  fs.writeFileSync(JSON_PATH, data)
-  res.status(200).send({message: '更新成功'})
+  setTableData('phone', data)
+  res.status(200).send({ message: '更新成功' })
 })
 
 app.get('/remove', function (req, res) {
-  const {id} = req.query
-  let raw = fs.readFileSync(JSON_PATH)
-  let list = JSON.parse(raw)
-  const index = list.findIndex(item => item.id === id)
-  const newList = [...list.slice(0, index), ...list.slice(index + 1, list.length)]
-  let data = JSON.stringify(newList)
-  fs.writeFileSync(JSON_PATH, data)
-  res.status(200).send({message: '删除成功'})
+  const { id } = req.query
+  const data = getTableData('phone')
+  const index = data.findIndex((item) => item.id === id)
+  const newList = [...data.slice(0, index), ...data.slice(index + 1, data.length)]
+  setTableData('phone', newList)
+  res.status(200).send({ message: '删除成功' })
 })
 
 app.listen(3000, console.log('==> server listen:3000'))
